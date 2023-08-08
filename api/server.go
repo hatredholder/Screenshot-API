02b60e2cb194b5264a8f3b864c02be0ba3ccf893
hatredholder <rattlingserpent@gmail.com>
2023-08-08 14:52:55 +0300

    add ServeMux for Server

--- api/server.go
+++ api/server.go
@@ -4,8 +4,20 @@ import (
 	"net/http"
 )
 
+func NewServeMux() *http.ServeMux {
+	sm := http.NewServeMux()
+
+	sm.Handle("/storage/", StorageHandler())
+	sm.HandleFunc("/screenshot/", ScreenshotWebsiteHandler)
+
+	return sm
+}
+
 func NewServer(addr string) *http.Server {
+	sm := NewServeMux()
+
 	return &http.Server{
-		Addr: addr,
+		Addr:    addr,
+		Handler: sm,
 	}
 }
